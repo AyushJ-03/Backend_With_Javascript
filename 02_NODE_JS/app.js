d35e380db41ee931af3e638ad404e1154272b07a
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-15 18:23:22 +0530

    Learn Response

--- 02_NODE_JS/app.js
+++ 02_NODE_JS/app.js
@@ -7,7 +7,30 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.methods, req.headers);
-    process.exit();
+
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete Coding</title></head>');
+        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    } else if (req.url === '/products') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete Coding</title></head>');
+        res.write('<body><h1>checkout our products!</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else{
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Complete Coding</title></head>');
+        res.write('<body><h1>Like / Share / Subscribe</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 const PORT = 3000;
