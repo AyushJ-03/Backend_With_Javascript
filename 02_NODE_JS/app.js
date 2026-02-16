4dbefe8aa5a24c171abda3eb580e19aa7ab8b0c3
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-16 15:48:04 +0530

    Before Project

--- 02_NODE_JS/app.js
+++ 02_NODE_JS/app.js
@@ -1,36 +1,38 @@
 const http = require('http');
-
-// function requestListener(req, res) { 
-//     console.log(req);
-// }
-// http.createServer(requestListener);
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.methods, req.headers);
+    console.log(req.url, req.method, req.headers);
 
     if (req.url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Complete Coding</title></head>');
-        res.write('<body><h1>Welcome to Home</h1></body>');
+        res.write('<body><h1>Enter Your Details:</h1>');
+        res.write('<form action="/submit-details" method="POST">');
+        res.write('<input type="text" name = "username" placeholder="Enter your name"><br>');
+        res.write('<label for="male">Male</label>');
+        res.write('<input type="radio" id="male" name="gender" value ="male" />');
+        res.write('<label for="female">Female</label>');
+        res.write('<input type="radio" id="female" name="gender" value ="female" />');
+        res.write('<br><input type="submit" value="Submit">');
+        res.write('</form>');
+        res.write('</body>');
         res.write('</html>');
         return res.end();
-    } else if (req.url === '/products') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Complete Coding</title></head>');
-        res.write('<body><h1>checkout our products!</h1></body>');
-        res.write('</html>');
-        return res.end();
+
+    } else if (req.url.toLowerCase() === "/submit-details" && req.method.toLowerCase() === "post") {
+        fs.writeFileSync('user.txt', 'Ayush Jaiswal');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
     }
-    else{
+
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Complete Coding</title></head>');
         res.write('<body><h1>Like / Share / Subscribe</h1></body>');
         res.write('</html>');
         return res.end();
-    }
 });
 
 const PORT = 3000;
