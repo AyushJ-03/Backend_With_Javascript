56cde7ea6fb6490a5829156b56d5bde27b719b6d
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-18 16:03:30 +0530

    Practice start

---
+++ EXPRESS/01_express_js/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const requestHandler = require('./user');
+
+const app = express();
+
+app.use('/',(req, res, next) => {
+  console.log("Came in first middleware !", req.url, req.method);
+  next();
+});
+
+app.use('/submit-details',(req, res, next) => {
+  console.log("Came in second middleware !", req.url, req.method);
+  res.send("<p>Welcome to Ayush's page");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`)
+});
+
