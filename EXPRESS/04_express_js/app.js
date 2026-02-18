8dc538995d78ec12a89d958eed959c1a73391efa
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-18 19:57:29 +0530

    Before Views

---
+++ EXPRESS/04_express_js/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use('/host', hostRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>404 Your page is not found on airbnb </h1>`);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('Server running on address http://localhost:' + PORT);
+});
