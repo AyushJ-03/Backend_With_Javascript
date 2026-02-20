021110015fe8567a5d0bd9d0bdec0eea75d4a5ea
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-21 01:03:38 +0530

    Complete Practice Set

---
+++ dynamic-ui-practice/routes/hostRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const hostRouter = express.Router();
+
+hostRouter.get('/add-home', (req, res, next) => {
+  res.render('addHome', {pageTitle : 'Add Home To airbnb'});
+});
+
+const registeredHomes = [];
+
+hostRouter.post('/add-home', (req, res, next) => {
+  console.log(req.body);
+  registeredHomes.push(req.body);
+  res.render('homeAdded', {pageTitle : 'Home Added Successfully'});
+});
+
+exports.hostRouter = hostRouter;
+exports.registeredHomes = registeredHomes;
